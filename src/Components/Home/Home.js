34026439060ec63d7d0d5aa789166aea57e849e8
homeboy445 <akshitsan13@gmail.com>
2021-04-29 18:53:07 +0530

    Made some minor UI upgrades

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,7 @@
-import React,{ useEffect } from 'react';
+import React,{ useEffect,useRef } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import samurai from '../../assets/samurai.jpg';
 import samurai1 from '../../assets/samurai1.jpg';
 import pointer from '../../assets/pointer2.png';
@@ -9,6 +11,13 @@ import './Home.css';
 
 const Home = ({moveup}) => {
 
+    gsap.registerPlugin(ScrollTrigger);
+    const ref1=useRef(null);
+    const ref2=useRef(null);
+    const ref3=useRef(null);
+    const ref4=useRef(null);
+    const ref5=useRef(null);
+
     useEffect(() =>{
         window.onload=()=>{
             scroll.scrollToTop();
@@ -16,6 +25,40 @@ const Home = ({moveup}) => {
         if(moveup){
             scroll.scrollToTop();
         }
+        gsap.to(ref1.current,
+        {
+            scrollTrigger:{
+                target:ref1.current,
+                toggleActions:"restart none none none"
+            },
+            opacity:1,
+            scale:1,
+            delay:0.1,
+            ease:"power3",
+            duration:0.4
+        });
+        gsap.from(ref2.current,{
+            scrollTrigger:{
+                target:ref2.current,
+                toggleActions:"restart none none none"
+            },
+            opacity:0,
+            width:"0%",
+            delay:1,
+            ease:"power3",
+            duration:1
+        });
+        gsap.from(ref3.current,{
+            scrollTrigger:{
+                target:ref3.current,
+                toggleActions:"restart none none none"
+            },
+            opacity:0,
+            width:"0%",
+            delay:3,
+            ease:"power3",
+            duration:4
+        });
     },[moveup]);
 
     return (
@@ -50,12 +93,12 @@ const Home = ({moveup}) => {
                 </div>
             </div>:null}
             <div className="Home_4">
-                    <h1>Give us your text and leave the rest to the Warrior.</h1>
-                    <div className="Home4bx1">
+                    <h1 ref={ref1}>Give us your text and leave the rest to the Warrior.</h1>
+                    <div className="Home4bx1" ref={ref2}>
                         <h1>Be it summarizing your text.</h1>
                         <img src={SummaryJAP} alt=""/>
                     </div>
-                    <div className="Home4bx2">
+                    <div className="Home4bx2" ref={ref3}>
                         <img src={CallJP} alt=""/>
                         <h1>Be it you wanting to vocabularize your text.</h1>
                     </div>
@@ -65,7 +108,7 @@ const Home = ({moveup}) => {
                 <img src={samurai1} alt="" className="Home5Img1"/>
             </div>
         </div>
-    )
+    );
 }
 
 export default Home;
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React,{ useState,useEffect,useRef } from 'react';
+import React,{ useEffect,useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Star from '../../assets/shuriken.svg';
